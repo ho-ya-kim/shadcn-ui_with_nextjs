2d20436ae1fc571f58f0c0eb567f6baf6de7beef
ho-ya-kim <seoho7777.kim@gmail.com>
2024-10-05 04:59:57 +0900

    setup

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,42 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link";
+
+export default function _Navigation() {
+    return (
+        <NavigationMenu className={"min-w-full"}>
+            <NavigationMenuList className={"min-w-full justify-around"}>
+                <NavigationMenuItem>
+                    <Link href={'/'} legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Documentation
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href={'/'} legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Documentation
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href={'/'} legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Documentation
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
